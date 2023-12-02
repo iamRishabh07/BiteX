02c014f3232d51407500368b4d5b113edbe83f86
iamRishabh07 <iamrishabh799@gmail.com>
2023-12-02 17:54:06 +0530

    added header(logo and nav items)

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 /*
  Header
   - Logo
@@ -18,6 +17,34 @@ Footer
  - Contact
 */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://cdn.dribbble.com/users/630677/screenshots/3833541/media/201454f743f48c415a38c49419275692.jpg?resize=800x600&vertical=center"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-root.render();
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />;
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);
